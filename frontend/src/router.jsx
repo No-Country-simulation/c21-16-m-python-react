4cfbb6e6f4ba00e6bf0dda7517c8782264deb536
Carlos Ayala <infocarlosayala@gmail.com>
2024-10-11 13:57:39 -0300

    feat: add icon library

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,25 +1,12 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  useRouteError,
-} from "react-router-dom";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import * as Home from "./routes";
 import * as Signin from "./routes/auth/signin";
 import * as Signup from "./routes/auth/signup";
-
-const ErrorBoundary = () => {
-  let error = useRouteError();
-  console.error(error);
-
-  return <div>Dang!</div>;
-};
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home.Page />,
-    errorElement: <ErrorBoundary />,
   },
   {
     path: "/auth/signin",
