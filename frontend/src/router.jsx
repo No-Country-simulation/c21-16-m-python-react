99373312db8382604ca89395a9f311b50a51ec6a
Carlos Ayala <infocarlosayala@gmail.com>
2024-10-13 15:22:22 -0300

    feat: setup auth, add alias path, toaster, env-vars, prettier and docs in readmi

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -2,22 +2,31 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import * as Home from "./routes";
 import * as Signin from "./routes/auth/signin";
 import * as Signup from "./routes/auth/signup";
+import { RedirectAuthenticated } from "./shared/components";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home.Page />,
-  },
-  {
-    path: "/auth/signin",
-    element: <Signin.Page />,
-  },
-  {
-    path: "/auth/signup",
-    element: <Signup.Page />,
-  },
+	{
+		path: "/",
+		element: <Home.Page />,
+	},
+	{
+		path: "/auth/signin",
+		element: (
+			<RedirectAuthenticated>
+				<Signin.Page />
+			</RedirectAuthenticated>
+		),
+	},
+	{
+		path: "/auth/signup",
+		element: (
+			<RedirectAuthenticated>
+				<Signup.Page />
+			</RedirectAuthenticated>
+		),
+	},
 ]);
 
 export const Routes = () => {
-  return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />;
 };
