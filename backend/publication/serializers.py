d37c4f2e79429489e4fb7ab093b337c826efd55c
FELIPE <felipe3133770868@gmail.com>
2024-10-16 18:00:25 -0500

    fix: Issue #16 /posts/user-publications/ endpoint receives files

--- backend/publication/serializers.py
+++ backend/publication/serializers.py
@@ -14,17 +14,26 @@ class FilesSerializer(serializers.ModelSerializer):
 # Define un serializer para el modelo "Publication" que gestiona las publicaciones.
 class PublicationSerializer(serializers.ModelSerializer):
     # Relaciona las publicaciones con sus archivos. El parámetro "many=True" indica que una publicación puede tener muchos archivos.
-    # "read_only=True" significa que los archivos no se pueden modificar directamente desde este serializer.
     files = FilesSerializer(many=True, required=False)
 
     # Define qué campos del modelo "Publication" se enviarán en la respuesta.
     class Meta:
         model = Publication
         # Incluye los campos de la publicación.
-        fields = ['id', 'content', 'files', 'publication_date']
+        files = ['id', 'content', 'files', 'publication_date']
         read_only_fields = ['id', 'publication_date']
 
     def create(self, validated_data):
+        files_data = validated_data.pop('files', [])
         request = self.context.get('request')
         user = request.user
-        return Publication.objects.create(id_user=user, **validated_data)
+
+        publication = Publication.objects.create(
+            id_user=user, **validated_data)
+
+        # Si se han enviado archivos, crea instancias de Files y asócialas a la publicación
+        for file_data in files_data:
+            file_instance = Files.objects.create(**file_data)
+            publication.files.add(file_instance)
+
+        return publication
